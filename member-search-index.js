b3d1a15eb97a3d5e953d96533f6a79fce13cfe65
Evgeniy0y0 <148543433+Evgeniy0y0@users.noreply.github.com>
2026-04-21 14:17:05 +0000

    Deploying to gh-pages from @ Evgeniy0y0/Project-@9bb52cd958990cd663874b4e1d643a898361e4a1 🚀

--- member-search-index.js
+++ member-search-index.js
@@ -1,1 +1,1 @@
-memberSearchIndex = [{"p":"org.example","c":"Main","l":"Main()","u":"%3Cinit%3E()"}];updateSearchResults();
+memberSearchIndex = [{"p":"org.example","c":"Main","l":"Main()","u":"%3Cinit%3E()"},{"p":"org.example","c":"Main","l":"testMethod()"}];updateSearchResults();
