9bb52cd958990cd663874b4e1d643a898361e4a1
Evgeniy0y0 <sievati.yevhen@student.sumdu.edu.ua>
2026-04-21 17:16:25 +0300

    Test JavaDoc

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,10 @@ package org.example;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    /**
+     * Test method
+     */
+    public void testMethod() {}
     static void main() {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
